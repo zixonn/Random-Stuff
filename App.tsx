e25240f2a4deb281d1d1f9faf2d2e7cb7ec8ba1a
Zixon77 <chikaosro@gmail.com>
2025-01-15 08:00:12 -0500

    quotes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [quotesArray, setQuotesArray] = useState([])
+  const [quote, setQuote] = useState("")
+
+  useEffect(() =>{
+    async function fetchQuotes(){
+      try{
+        fetch("https://zenquotes.io/api/quotes/")
+        .then(response => response.json())
+        .then(data => setQuotesArray(data))
+      } catch (error){
+        console.log(error)
+      }
+    }
+    fetchQuotes()
+  },[])
+
+  function generateQuote(){
+    if (quotesArray.length > 0) {
+      const random = Math.floor(Math.random() * quotesArray.length) 
+      const selectedQuote = quotesArray[random];
+      setQuote(`${selectedQuote.q} - ${selectedQuote.a}`); 
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style = {styles.quote}>{quote}</Text>
+      <Button title='Get Quote' onPress={generateQuote} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +43,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding:"5%"
   },
+  quote:{
+    textAlign:"center",
+    marginVertical:"3%"
+  }
 });
